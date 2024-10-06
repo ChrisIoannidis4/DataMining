590dd4c0e503b6f0dc45801330d183d449e8f394
Dina Bellouki <dinab2002@gmail.com>
2024-10-06 22:15:45 +0200

    outputs splits

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def define_splits(x, y):
     for c in splitpoints:
         left_ch = y[x<=c]
         right_ch = y[x>c]
-        splits[c] = (len(left_ch)/len(y))*gini_index(left_ch) + (len(left_ch)/len(y))*gini_index(right_ch)
+        splits[c] = (len(left_ch)/len(y))*gini_index(left_ch) + (len(right_ch)/len(y))*gini_index(right_ch)
 
     return splits
 
@@ -69,8 +69,9 @@ def majority_class(y):
     # return np.argmax(np.bincount(y)) # this is used for leaf node's utility of predicting the majority class
 
 
-def tree_grow(x, y, nmin, minleaf, nfeat):
-
+def tree_grow(x, y, nmin, minleaf, nfeat, feat, index):
+    # if index == 2:
+    #     return TreeNode(label=majority_class(y))
     if len(y) < nmin or len(set(y)) == 1:  #stopping if pure node or fewer than nmin samples, creates leaf node
         return TreeNode(label=majority_class(y))
 
@@ -94,10 +95,17 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
     #now we will split the node's data to the two children
     left_mask = x[:, best_feature] > best_split_point
     right_mask =  x[:, best_feature] <= best_split_point
+    if left_mask.sum() < minleaf or right_mask.sum() < minleaf:
+        return TreeNode(label=majority_class(y))
+    print(feat[feature_index])
+    print(best_split_point)
+    print(f"left child {len(x[left_mask])}")
+    print(f"right child {len(x[right_mask])}")
+    # print(f"total: {x}")
 
     #recursively grow a tree for each child, which will be in reality a branch of the initial tree
-    left_child = tree_grow(x[left_mask], y[left_mask], nmin, minleaf, nfeat)
-    right_child = tree_grow(x[right_mask], y[right_mask], nmin, minleaf, nfeat)
+    left_child = tree_grow(x[left_mask], y[left_mask], nmin, minleaf, nfeat, feat, index+1)
+    right_child = tree_grow(x[right_mask], y[right_mask], nmin, minleaf, nfeat, feat, index+1)
     tree = TreeNode(feature=best_feature, split_thr=best_split_point, left_child=left_child, right_child=right_child)
 
     return tree
@@ -135,42 +143,6 @@ def draw_sample(x, y):
     y_sampled = np.array([y[i] for i in sample_idx])
 
     return x_sampled, y_sampled
-
-
-def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
-    """
-    Draws m bootstrap samples and returns a list of m trees
-
-    :x: attribute values
-    :y: class labels
-    :nmin: minimum number of observations in node for split
-    :minleaf: minimum number of observations required for a leaf node
-    :nfeat: number of features considered for each split 
-    :m: number of bootstrap samples to be drawn
-    """    
-    ensemble = []
-    for i in range(m):
-        x_sampled, y_sampled = draw_sample(x, y)
-        ensemble.append(tree_grow(x_sampled, y_sampled, nmin, minleaf, nfeat))
-
-    return ensemble
-
-
-def tree_pred_b(x, trees):
-    """
-    Gets predictions from m trees for all data points
-
-    :x: 2D numpy array (n_samples, n_features) 
-    :tr: The decision tree object (root node) created in tree_grow
-    """
-    votes = [tree_pred(x, tr) for tr in trees]
-
-    # Transpose so that every row has the votes for a sample 
-    votes_array =  np.vstack(votes).T
-    
-    preds = [np.argmax(np.bincount(votes_array[i])) for i in range(votes_array.shape[0])]
-
-    return np.array(preds)
 
 
 def write_results(filename, train_preds, y_train, test_preds, y_test):
@@ -213,8 +185,9 @@ def write_results(filename, train_preds, y_train, test_preds, y_test):
 
 
 # load train and test data from csv files
-data = np.genfromtxt('./eclipse_train.csv', delimiter=',', skip_header=1)
-test_data = np.genfromtxt('./eclipse_test.csv', delimiter=',', skip_header=1)
+data = np.genfromtxt('./pima_data.csv', delimiter=',', skip_header=1)
+test_data = np.genfromtxt('./pima_data.csv', delimiter=',', skip_header=1)
+features = np.genfromtxt('./pima_data.csv', delimiter=',', max_rows=1, dtype=str)
 
 # train data
 x_train = data[:, :-1]
@@ -224,19 +197,12 @@ x_test = test_data[:, :-1]
 y_test = test_data[:,-1]
 
 # Single tree
-tr = tree_grow(x_train, y_train, 15, 5, 41)
+tr = tree_grow(x_train, y_train, 20, 5, 8, features, 0)
+print(len(x_train[:, 0]))
+print(tr)
+# print(best_split(x_train[:, 0], y_train))
 preds_train = tree_pred(x_train, tr)
 preds_test = tree_pred(x_test, tr)
 write_results('single_tree', preds_train, y_train, preds_test, y_test)
-# Bagging
-trees = tree_grow_b(x_train, y_train, 15, 5, 41, 100)
-preds_train_bag = tree_pred_b(x_train, trees)
-preds_test_bag = tree_pred_b(x_test, trees)
-write_results('bagging', preds_train_bag, y_train, preds_test_bag, y_test)
-# Random Forest
-forest = tree_grow_b(x_train, y_train, 15, 5, 6, 100)
-preds_train_for = tree_pred_b(x_train, forest)
-preds_test_for = tree_pred_b(x_test, forest)
-write_results('random_forest', preds_train_for, y_train, preds_test_for, y_test)
 
 
