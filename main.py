332706ad03c91265f28af77190483c5ffbca4105
Dina Bellouki <dinab2002@gmail.com>
2024-10-07 01:30:38 +0200

    Smaller deviation

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, confu
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(suppress=True)
+
 class TreeNode:
     def __init__(self, feature=None, split_thr=None, left_child=None, right_child=None, label=None, is_root = False):
         self.feature = feature       # index for the feature for which the node splits
@@ -12,19 +15,9 @@ class TreeNode:
         self.left_child = left_child 
         self.right_child = right_child # these (left/right child) point to the children    
         self.label = label           # if it is leaf, it will show what class/label it predicts
-        # self.is_root = is_root    
+   
     def is_leaf(self):
         return self.label is not None  # boolean, if it is leaf or not (so that we know if it is the classifier node)
-
-# class DecisionTree:
-#     def __init__(self, rootNode=None):
-#         self.rootNode=rootNode   #the decision tree's root node 
-
-
-# def impurity(y_array):
-#     num_zeros = (y_array == 0).sum()
-#     num_ones = (y_array == 1).sum()
-#     return (num_zeros*num_ones)/(y_array.size **2)
 
 def gini_index(y_array):
     num_zeros = (y_array == 0).sum()
@@ -36,9 +29,8 @@ def gini_index(y_array):
     p_1 = num_ones / total
     return 1 - (p_0 ** 2 + p_1 ** 2)
 
-
 # Returns splits: dictionary of the possible splitting point and the calculated impurity for them 
-def define_splits(x, y):
+def define_splits(x, y, minleaf):
     sorted_col = np.sort(np.unique(x))
     splitpoints = (sorted_col[0:-1] + sorted_col[1:])/2
 
@@ -46,18 +38,24 @@ def define_splits(x, y):
     for c in splitpoints:
         left_ch = y[x<=c]
         right_ch = y[x>c]
-        splits[c] = (len(left_ch)/len(y))*gini_index(left_ch) + (len(left_ch)/len(y))*gini_index(right_ch)
+
+        #If minleaf constraint is violated, rather than possibly choosing this split
+        #resulting in an immediate leaf, setting the value to 1 allows for the program
+        #to still look for other splits
+        if left_ch.size < minleaf or right_ch.size < minleaf:
+            splits[c] = 1
+            continue
+        
+        splits[c] = (len(left_ch)/len(y))*gini_index(left_ch) + (len(right_ch)/len(y))*gini_index(right_ch)
 
     return splits
 
-
-def best_split(x, y):
-    split_dict = define_splits(x,y)
+def best_split(x, y, minleaf):
+    split_dict = define_splits(x, y, minleaf)
     if not split_dict:
         return None, None # if no splitting points are found for the particular feature, pass 
     split_value = min(split_dict, key=split_dict.get) #the key in the dictionary (splitting point) for which the value (impurity) is minimized
     return split_value, split_dict[split_value]
-
 
 def majority_class(y):
     num_zeros = (y == 0).sum()
@@ -68,9 +66,7 @@ def majority_class(y):
         return 0
     # return np.argmax(np.bincount(y)) # this is used for leaf node's utility of predicting the majority class
 
-
 def tree_grow(x, y, nmin, minleaf, nfeat):
-
     if len(y) < nmin or len(set(y)) == 1:  #stopping if pure node or fewer than nmin samples, creates leaf node
         return TreeNode(label=majority_class(y))
 
@@ -79,7 +75,7 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
     best_feature, best_split_point, best_impurity = None, None, 1 #initialization, 1 is more than the max gini could be
 
     for feature_index in features_to_consider: # to find the best splitting point across all features
-        split_point, impurity_value = best_split(x[:, feature_index], y) #best splitting point for each feature
+        split_point, impurity_value = best_split(x[:, feature_index], y, minleaf) #best splitting point for each feature
         if split_point is None:
             continue
 
@@ -92,8 +88,8 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
         return TreeNode(label=majority_class(y))    # if no splitting point is found, then we have a leaf node
     
     #now we will split the node's data to the two children
-    left_mask = x[:, best_feature] > best_split_point
-    right_mask =  x[:, best_feature] <= best_split_point
+    left_mask = x[:, best_feature] <= best_split_point
+    right_mask =  x[:, best_feature] > best_split_point
 
     #recursively grow a tree for each child, which will be in reality a branch of the initial tree
     left_child = tree_grow(x[left_mask], y[left_mask], nmin, minleaf, nfeat)
@@ -105,7 +101,7 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
 
 def predict_single(instance, node):
     while not node.is_leaf(): #go through the nodes until we reach the leaf one that is the final classifier
-        if instance[node.feature] > node.split_thr:
+        if instance[node.feature] <= node.split_thr:
             node = node.left_child 
         else:
             node = node.right_child 
@@ -116,7 +112,6 @@ def tree_pred(x, tr):
     """
     x: 2D numpy array (n_samples, n_features) 
     tr: The decision tree object (root node) created in tree_grow
-    y: 1D numpy array of predicted class labels
     """
     return np.array([predict_single(row, tr) for row in x])
 
@@ -211,7 +206,6 @@ def write_results(filename, train_preds, y_train, test_preds, y_test):
 
     return
 
-
 # load train and test data from csv files
 data = np.genfromtxt('./eclipse_train.csv', delimiter=',', skip_header=1)
 test_data = np.genfromtxt('./eclipse_test.csv', delimiter=',', skip_header=1)
@@ -238,5 +232,3 @@ forest = tree_grow_b(x_train, y_train, 15, 5, 6, 100)
 preds_train_for = tree_pred_b(x_train, forest)
 preds_test_for = tree_pred_b(x_test, forest)
 write_results('random_forest', preds_train_for, y_train, preds_test_for, y_test)
-
-
